7e3e850f88767e09bcb9aed1c2b69c895ecb072c
liqiantu <867899639@qq.com>
2019-10-07 02:12:23 +0800

    初始化项目

---
+++ MagazineReader/MagazineReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagazineReader
+//
+//  Created by liqiantu on 2019/10/7.
+//  Copyright © 2019 liqiantu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MagazineReader/Pods/Target Support Files/Pods-MagazineReader/Pods-MagazineReader-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MagazineReader : NSObject
+@end
+@implementation PodsDummy_Pods_MagazineReader
+@end
