2043496fd7f4a2d66913b670cb2d0e8b66000ad7
liqiantu <867899639@qq.com>
2019-10-09 17:49:46 +0800

    1.初始化

---
+++ MagazineReader/MagazineReader/Component/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  MagazineReader
+//
+//  Created by liqiantu on 2019/10/9.
+//  Copyright © 2019 liqiantu. All rights reserved.
+//
+
+import Foundation
---
+++ MagazineReader/MagazineReader/Model/Model.swift
@@ -0,0 +1,10 @@
+//
+//  Model.swift
+//  MagazineReader
+//
+//  Created by liqiantu on 2019/10/9.
+//  Copyright © 2019 liqiantu. All rights reserved.
+//
+
+import Foundation
+import HandyJSON
